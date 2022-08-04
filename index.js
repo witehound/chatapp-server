a16c7fdc0fd366d7905c512ee963ab9d0112bcc8
InventedMost <netvid00@gmail.com>
2022-08-04 15:48:34 +0100

    added all usefull socket.io events

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { nanoid } from "nanoid";
-import io from "./utils/socketio.js";
+import startIo from "./utils/socketio.js";
 
 const app = express();
 app.use(cors());
 const port = process.env.PORT;
 
-const users = [];
-const rooms = [];
+startIo();
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
--- utils/socketio.js
+++ utils/socketio.js
@@ -1,6 +1,6 @@
-import { Socket } from "dgram";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { nanoid } from "nanoid";
 
 const httpServer = createServer();
 const options = {
@@ -11,4 +11,50 @@ const options = {
 
 const io = new Server(httpServer, options);
 
-export default io;
+const users = [];
+const rooms = [];
+
+const startIo = () => {
+  io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+    users.push(socket.id);
+    socket.broadcast.emit("updateUsers", users);
+
+    socket.on("disconnect", () => {
+      users = users.filter((user) => (user = !socket.id));
+      socket.broadcast.emit("updateUsers", users);
+      socket.disconnect();
+    });
+
+    socket.emit("getAllUsers", users);
+
+    socket.on("createRoom", () => {
+      const room = {
+        id: nanoid(7),
+        chat: [],
+      };
+
+      socket.join(room);
+      socket.emit("getRoom", room);
+      rooms.push(room);
+      socket.broadcast.emit("updateRooms", rooms);
+    });
+
+    socket.on("joinRoom", (room) => {
+      socket.join(room);
+    });
+    socket.broadcast.emit("updateRooms", rooms);
+
+    socket.on("message", (payload) => {
+      rooms.map((room) => {
+        if (room.id === payload.room) {
+          singleChat = { message: payload.message, writer: payload.socketId };
+          room.chat.push(singleChat);
+        }
+      });
+      io.to(payload.room).emit("chat", payload);
+    });
+  });
+};
+
+export default startIo;
